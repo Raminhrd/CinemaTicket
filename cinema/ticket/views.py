7ddc02667c8d6d369031753400500b3752f7d0a5
raminhrd <raminhr.work@gmail.com>
2025-09-23 13:23:06 +0330

    add_FilterSearch

--- cinema/ticket/views.py
+++ cinema/ticket/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from ticket.serializer import *
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend, OrderingFilter
 from ticket.models import Cinema, Movie, Ticket, Actor, Comment, Genre,  Criticism
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
@@ -8,21 +10,34 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 class CinemaListCreate(ListCreateAPIView):
     queryset = Cinema.objects.all()
     serializer_class = CinemaSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name']
+    ordering_fields = ['capacity']
 
 
 class CinemaRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
     queryset = Cinema.objects.all()
     serializer_class = CinemaRetrieveUpdateDestroySerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['name']
 
 
 class MovieListCreate(ListCreateAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    search_fields = ['name']
+    filterset_fields = ['genres']
+    ordering_fields = ['price', 'score', 'votes']
 
 
 class MovieRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
     queryset = Movie.objects.all()
     serializer_class = MovieRetrieveUpdateDestroySerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    search_fields = ['name']
+    filterset_fields = ['genres']
+    ordering_fields = ['price', 'score', 'votes']
 
 
 class SansListCreate(ListCreateAPIView):
@@ -48,11 +63,15 @@ class TicketRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
 class ActorListCreate(ListCreateAPIView):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['name']
 
 
 class ActorRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
     queryset = Actor.objects.all()
     serializer_class = ActorRetrieveUpdateDestroySerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['name']
 
 
 class CommentListCreate(ListCreateAPIView):
@@ -68,11 +87,15 @@ class CommentRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
 class GenreListCreate(ListCreateAPIView):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['name']
 
 
 class GenreRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
     queryset = Genre.objects.all()
     serializer_class = GenreRetrieveUpdateDestroySerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['name']
 
 
 class CriticismListCreate(ListCreateAPIView):
